eb407924177d6ea30a471a7e47fe80327d4cb5ab
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-10-31 14:29:44 -0300

    Organização de pastas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
 import "./App.css";
-import InputText from "./componentes/InputText";
-import Lista from "./componentes/Lista";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div id="main">
       <h1 className="titulo">Lista de Tarefas</h1>
-      <InputText />
-      <Lista />
+
+      <div className="input-text">
+        <input
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value);
+          }}
+          type="text"
+          placeholder="Adicione uma terefa"
+        />
+        <button onClick={() => btnExcluir()}>Add</button>
+      </div>
+
+      <div className="lista">
+        <img width={320} src="./tarefas.svg" alt="Imagem de uma lista de tarefas." />
+      </div>
     </div>
   );
 }
--- src/componentes/Lista.jsx
+++
@@ -1,9 +0,0 @@
-function Lista() {
-  return (
-    <div className="lista">
-      <img width={320} src="./tarefas.svg" alt="Imagem de uma lista de tarefas." />
-    </div>
-  );
-}
-
-export default Lista;
