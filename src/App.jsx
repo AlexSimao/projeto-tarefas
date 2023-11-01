934e4ccc107aa43d3f19d47f32bb5efb48224a82
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-11-01 17:31:58 -0300

    Salvando os dados em localStorage. Projeto Fializado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Item from "./componentes/Item";
 import Icon from "./assets/tarefas.svg";
 
 function App() {
+  function salvarLocalStorage(chave, valor) {
+    localStorage.setItem(chave, JSON.stringify(valor));
+  }
+
+  function lerLocalStorage(chave) {
+    return JSON.parse(localStorage.getItem(chave));
+  }
+
   const [inputText, setInputText] = useState("");
-  const [lista, setLista] = useState([]);
+  const [lista, setLista] = useState(lerLocalStorage("lista") ?? []);
+
+  useEffect(() => {
+    salvarLocalStorage("lista", lista);
+  }, [lista]);
 
   function addItem(form) {
     form.preventDefault();
     if (!inputText) {
-      console.log(lista);
       return;
     }
 
@@ -38,7 +49,7 @@ function App() {
       <div className="lista">
         {lista.length < 1 ? <img className="img-tarefas" src={Icon} /> : lista.map((item, index) => <Item setLista={setLista} lista={lista} index={index} item={item} />)}
 
-        {lista.length > 0 ? (
+        {lista.length > 0 && (
           <button
             onClick={() => {
               setLista([]);
@@ -47,8 +58,6 @@ function App() {
           >
             Remover Todos
           </button>
-        ) : (
-          ""
         )}
       </div>
     </div>
